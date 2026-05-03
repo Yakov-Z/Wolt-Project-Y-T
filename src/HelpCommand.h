d3d97935efa5782bc23c7a4f28161aa6b0cb4680
Yaakov Zana <hgeczbt@gmail.com>
2026-05-03 21:24:31 +0300

    PWY-16 fixed help - make it generic

--- src/HelpCommand.h
+++ src/HelpCommand.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "ICommand.h"
 #include "IOutputWriter.h"
+#include <string>
 
 class HelpCommand : public ICommand {
 private:
     IOutputWriter& writer;
+    const std::string& helpText;
 public:
-    HelpCommand(IOutputWriter& outputWriter);
+    HelpCommand(IOutputWriter& outputWriter, const std::string& helpText);
     void execute() override;
 };
