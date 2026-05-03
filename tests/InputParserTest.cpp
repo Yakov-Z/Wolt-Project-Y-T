6faebb762fd8127f603796bd52dc52224170bf76
1923Tomer <Btomertomer@gmail.com>
2026-05-03 16:11:02 +0300

    PWY-52 - fix tests

--- tests/InputParserTest.cpp
+++ tests/InputParserTest.cpp
@@ -3,6 +3,7 @@
 #include "ICommand.h"
 #include <vector>
 #include <string>
+#include <sstream> 
 
 //check the parser on not real command
 class notrealCommand : public ICommand {
@@ -12,10 +13,23 @@ public:
     }
 };
 
+// simulate get input from the user
+ICommand* simulateInput(InputParser& parser, const std::string& simulatedInput) {
+    // /n in the end to simulate the enter
+    std::istringstream sim(simulatedInput + "\n"); 
+    std::streambuf* originalCinBuffer = std::cin.rdbuf(sim.rdbuf()); 
+    ICommand* commdand = parser.parseNextCommand(); 
+    //back to the regular buffer
+    std::cin.rdbuf(originalCinBuffer); 
+    
+    return commdand;
+}
+
 //deal with empty line
 TEST(InputParserTest, EmptyLine) {
     InputParser parser;
-    ICommand* command = parser.parseNextCommand("");
+    
+    ICommand* command = simulateInput(parser, "");
     
     EXPECT_EQ(command, nullptr);
 }
@@ -23,7 +37,8 @@ TEST(InputParserTest, EmptyLine) {
 //deal with illegal command
 TEST(InputParserTest, ilegalCommand) {
     InputParser parser;
-    ICommand* command = parser.parseNextCommand("chikooooo");
+    ICommand* command = simulateInput(parser, "chikooooo");
+    
     EXPECT_EQ(command, nullptr);
 }
 
@@ -35,7 +50,8 @@ TEST(InputParserTest, real_command) {
         return new notrealCommand(); 
     });
 
-    ICommand* command = parser.parseNextCommand("Hapoel_command 92 94");
+    ICommand* command = simulateInput(parser, "Hapoel_command 92 94");
+    
     EXPECT_NE(command, nullptr);
     delete command;
 }
@@ -45,7 +61,6 @@ TEST(InputParserTest, PassArguments) {
     InputParser parser;
     bool argsPass = false; 
 
-   
     parser.mapCommand("real_command", [&argsPass](const std::vector<std::string>& args) -> ICommand* {
         
         if (args.size() == 2 && args[0] == "51" && args[1] == "4") {
@@ -54,7 +69,7 @@ TEST(InputParserTest, PassArguments) {
         return new notrealCommand();
     });
 
-    ICommand* command = parser.parseNextCommand("real_command 51 4");
+    ICommand* command = simulateInput(parser, "real_command 51 4");
     
     EXPECT_NE(command, nullptr);
     EXPECT_TRUE(argsPass);
