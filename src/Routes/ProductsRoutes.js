c78553ade3763db27678959faed032639bbea4cd
Yaakov Zana <hgeczbt@gmail.com>
2026-05-28 18:15:41 +0300

    PWY-116 allow products routing access to restaurant id

--- src/Routes/ProductsRoutes.js
+++ src/Routes/ProductsRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const controller = require('../Controllers/RestaurantController');
 
 router.route('/')
     .get(controller.getRestaurantMenu)
     .post(controller.addProductToMenu);
 
-router.route('/:pld')
+router.route('/:pId')
     .get(controller.getProductById)
     .patch(controller.updateProduct)
     .delete(controller.deleteProduct);
