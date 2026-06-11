ce4aebd9e00e9ed576ab046fa4b504a737c5cb27
1923Tomer <Btomertomer@gmail.com>
2026-06-11 18:11:25 +0300

    PWY-164---change-JS-classes

--- src/Routes/RestaurantsRoutes.js
+++ src/Routes/RestaurantsRoutes.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../Controllers/RestaurantController');
+const searchController = require('../Controllers/SearchController');
 const ProductsRoutes = require('./ProductsRoutes');
+
 
 router.route('/')
     .get(controller.getAllRestaurants)
     .post(controller.createRestaurant);
 
+    router.get('/search/:query', controller.searchEntities);
+
+
+router.get('/category', controller.getExistingCategories);
+
+
+router.get('/category/:category', controller.getRestaurantsByCategory);
+
+
+router.get('/popular', controller.getPopularRestaurants);
+
+
 router.route('/:id')
     .get(controller.getRestaurantById)
     .patch(controller.updateRestaurant)
     .delete(controller.deleteRestaurant);
+
 
 router.use('/:id/products', ProductsRoutes);
 
