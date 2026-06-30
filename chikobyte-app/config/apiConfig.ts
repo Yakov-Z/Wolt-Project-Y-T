a7bdb6d5837c3dc0b35692b0264399650e0eeca6
Yaakov Zana <hgeczbt@gmail.com>
2026-06-30 11:49:51 +0300

    PWY-180 update IP to local for emulator

--- chikobyte-app/config/apiConfig.ts
+++ chikobyte-app/config/apiConfig.ts
@@ -6,6 +6,6 @@ const CURRENT_NETWORK_IP = '10.51.67.236';
 // Base URL configuration for all backend API fetch requests
 // Toggle the comments below depending on whether you are using a Physical Device or an Emulator
 export const BASE_URL = Platform.OS === 'android'
-  ? `http://${CURRENT_NETWORK_IP}:5000` // Use this for Physical Device (Hotspot or LAN)
-  // ? 'http://10.0.2.2:5000'           // Use this if you switch back to the Android Emulator
+  // ? `http://${CURRENT_NETWORK_IP}:5000` // Use this for Physical Device (Hotspot or LAN)
+  ? 'http://10.0.2.2:5000'           // Use this if you switch back to the Android Emulator
   : 'http://localhost:5000';            // Use this for iOS Simulator or Web
